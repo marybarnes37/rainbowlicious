903ebcfe8eef8233e6239361bd13a888042f05c7
Mary <marybarnes37@gmail.com>
2017-10-15 20:09:31 -0700

    added print messsages to monitor instascrap pt 2

--- instagram_scraper/instascrape_part2.py
+++ instagram_scraper/instascrape_part2.py
@@ -21,7 +21,7 @@ def setup_mongo_client(db_name, collection_name, client=None, address='mongodb:/
 
 
 def add_urls_and_datetimes(collection):
-    cursor = collection.find()
+    cursor = collection.find({ "url" : { "$exists" : False } })
     base_url = 'https://www.instagram.com/p/'
     for record in cursor:
         shortcode = record['node']['shortcode']
@@ -33,6 +33,8 @@ def add_urls_and_datetimes(collection):
 
 def visit_urls_get_locations(collection):
     cursor = collection.find({ "location_name" : { "$exists" : False } })
+    added_counter = 0
+    deleted_counter = 0
     for record in cursor:
         time.sleep(np.random.randint(8, 12))
         url = record['url']
@@ -46,39 +48,53 @@ def visit_urls_get_locations(collection):
                 location_name = location_info['name']
                 collection.update_one({"_id": record["_id"]}, {"$set": {'location_id': location_id}})
                 collection.update_one({"_id": record["_id"]}, {"$set": {'location_name': location_name}})
+                added_counter += 1
             except:
                 collection.delete_one({"_id": record["_id"]})
+                deleted_counter += 1
         else:
             with open('/Users/marybarnes/capstone_galvanize/rainbowlicious/instagram_scraper/status_code_log.txt', "a") as myfile:
                 myfile.write("status code: {}\n {} \n{}".format(r.status_code, r.content, r.headers))
             print('encountered status code {}'.format(r.status_code))
+            print("had already added {} raw locations and deleted {} records".format(added_counter, deleted_counter))
             return None
+        print("added {} raw locations and deleted {} records".format(added_counter, deleted_counter))
 
 
 def add_lat_long(collection):
-    cursor = collection.find()
+    cursor = collection.find({ "latitude" : { "$exists" : False } })
     geolocator = Nominatim()
+    added_counter = 0
+    deleted_counter = 0
     for record in cursor:
         geolocation = geolocator.geocode(record['location_name'],timeout=10000)
         if geolocation!=None:
             collection.update_one({"_id": record["_id"]}, {"$set": {'latitude': geolocation.latitude, 'longitude': geolocation.longitude}})
         else:
             collection.delete_one({"_id": record["_id"]})
+    print("added {} latitude and longitude and deleted {} records".format(added_counter, deleted_counter))
 
 
 def filter_US_locations(collection):
-    cursor = collection.find()
+    cursor = collection.find({ "location_dict" : { "$exists" : False } })
+    added_counter = 0
+    deleted_counter = 0
     for record in cursor:
         location_data = reverse_geocoder.search((record['latitude'], record['longitude']))
         if location_data[0]['cc'] == 'US':
             collection.update_one({"_id": record["_id"]}, {"$set": {'location_dict': location_data[0]}})
         else:
             collection.delete_one({"_id": record["_id"]})
+    print("added {} location dicts and deleted {} records".format(added_counter, deleted_counter))
 
 
 def main():
     setup_mongo_client('capstone', 'insta_rainbow')
     add_urls_and_datetimes(collection)
+    print('added urls_and_datetimes')
     visit_urls_get_locations(collection)
+    print('visited urls and got locations')
     add_lat_long(collection)
+    print('added longitude and latitude')
     filter_US_locations(collection)
+    print('filtered for US locations')
